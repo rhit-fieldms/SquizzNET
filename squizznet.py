caf0f02296b325fd6bf0ad83318bfa1c27fc4e8f
rhit-bostiadm <98357001+rhit-bostiadm@users.noreply.github.com>
2023-05-09 16:49:07 -0400

    there was an attempt

--- squizznet.py
+++ squizznet.py
@@ -11,36 +11,60 @@ from transformers import GPT2Tokenizer, GPT2Model
 
 print("squizz on them")
 
-def main(page: ft.Page):
+page = ft.Page
+qTopic = ''
+view = ''
+
+def quizzMake(page: ft.Page):
     page.title = "SquizzNET™"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
-    jamison = ft.Image(src=f"ezgif.com-crop.gif")
+    qTopic = ft.TextField()
     def button_clicked(e):
-        b.data += 1
-        t.value = f"Squizzed {b.data} time(s)"
-        page.update()
-
-    b = ft.ElevatedButton("Press to Squizz", on_click=button_clicked, data=0)
-    t = ft.Text()
+        print('peepee')
     page.add(
-        ft.Row(
+        ft.Row([
+                ft.Text(value="Provide a specific topic for your Squizz", text_align=ft.TextAlign.RIGHT)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER), 
+        ft.Row([qTopic], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([ft.ElevatedButton("Generate Squizz", on_click=button_clicked)], alignment=ft.MainAxisAlignment.CENTER)
+    )
+
+def displayQuiz(page: ft.Page):
+    print('funny')
+    # generator = pipeline('text-generation', model='gpt2')
+    # set_seed(42)
+    # generator("Make a quiz about " + qTopic, num_return_sequences=1)
+
+def welcomeToSquizz():
+    jamison = ft.Image(src=f"ezgif.com-crop.gif")
+    b = ft.ElevatedButton("Press to Squizz", on_click=button_clicked)
+    page.add(ft.Column(
+        horizontal_alignment = ft.MainAxisAlignment.CENTER,
+        controls = [
+            ft.Row(
             [
                 ft.Text(value="Welcome to SquizzNET™", text_align=ft.TextAlign.RIGHT)
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ), ft.Row([jamison], alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([b, t], alignment=ft.MainAxisAlignment.CENTER)
-    )
+        ft.Row([b], alignment=ft.MainAxisAlignment.CENTER)
+        ]    
+    ))
+    return view
+
+def button_clicked(e):
+        page.remove(view)
+        page.views.append(quizzMake)
+        #page.update()
+
+def main(page: ft.Page):
+    page.title = "SquizzNET™"
+    #view = welcomeToSquizz()
+    page.add(welcomeToSquizz())
+    # page.remove(view)
+    
 
 ft.app(target=main)
 
-generator = pipeline('text-generation', model='gpt2')
-set_seed(42)
-generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
-
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2Model.from_pretrained('gpt2')
-text = "Replace me by any text you'd like."
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
